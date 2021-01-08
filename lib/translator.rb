16465b1e9e9c368906b7dc393a807bc987106db6
sydney miranda <sydmiranda@icloud.com>
2021-01-08 23:31:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,6 +2,18 @@
 require "yaml"
 require 'pry'
 
+
+#def load_library(path)
+#final_hash = {}
+# YAML.load_file(path).each do |key, value|
+#final_hash[key] = {}
+#final_hash[key][:english] = value[0]
+# final_hash[key][:japanese] = value[1]
+#end
+#final_hash
+#end
+
+
 def load_library (file_path)
   file = YAML.load_file(file_path)
   dictionary = {}
