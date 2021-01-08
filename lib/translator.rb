72481ea993a3066610fada784e81d52fa30cb3f9
sydney miranda <sydmiranda@icloud.com>
2021-01-08 23:11:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,13 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
+def load_library(file_path)
+  file = YAML.load_file(file_path)
   # code goes here
+  file_path.each do |key, value|
+          binding.pry
+  end
 end
 
 def get_japanese_emoticon
