d20858e14b86fd3a90c820260c644e5ef80e33c1
sydney miranda <sydmiranda@icloud.com>
2021-01-08 23:15:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,6 +20,16 @@ end
 
 def get_japanese_emoticon
   # code goes here
+  dictionary = load_library(file_path)
+  eng_name = ""
+  dictionary.each do |name, value|
+    if emoji == value[:japanese]
+       return eng_name = name
+    else
+      eng_name = "Sorry, that emoticon was not found"
+    end
+  end
+  eng_name
 end
 
 def get_english_meaning(file_path, emoji)
