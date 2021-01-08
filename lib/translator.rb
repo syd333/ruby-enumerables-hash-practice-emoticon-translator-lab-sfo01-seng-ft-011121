0409aad0fd6d314a5a5639d631a30566bd1850ba
sydney miranda <sydmiranda@icloud.com>
2021-01-08 23:12:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,8 +2,20 @@
 require "yaml"
 require 'pry'
 
-def load_library(file_path)
+def load_library (file_path)
+  file = YAML.load_file(file_path)
+  dictionary = {}
+  file.each do |name, emoji_array|
+    dictionary[name] = {:english => {}, :japanese => {}}
+    emoji_array.each_with_index do |emoji, index|
+      if index == 0
+        dictionary[name][:english] = emoji
+      elsif index == 1
+        dictionary[name][:japanese] = emoji
+      end
+    end
   end
+  dictionary
 end
 
 def get_japanese_emoticon
