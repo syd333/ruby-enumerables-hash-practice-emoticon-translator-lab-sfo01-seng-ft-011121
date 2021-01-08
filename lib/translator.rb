fb7dfc8e85db52a8e507efa5bccc936389ad98bb
sydney miranda <sydmiranda@icloud.com>
2021-01-08 23:33:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,45 +14,10 @@ require 'pry'
 #end
 
 
-def load_library (file_path)
-  file = YAML.load_file(file_path)
-  dictionary = {}
-  file.each do |name, emoji_array|
-    dictionary[name] = {:english => {}, :japanese => {}}
-    emoji_array.each_with_index do |emoji, index|
-      if index == 0
-        dictionary[name][:english] = emoji
-      elsif index == 1
-        dictionary[name][:japanese] = emoji
-      end
-    end
-  end
-  dictionary
-end
-
-def get_japanese_emoticon(file_path, emoji)
-  # code goes here
-  dictionary = load_library(file_path)
-  eng_name = ""
-  dictionary.each do |name, value|
-    if emoji == value[:japanese]
-       return eng_name = name
-    else
-      eng_name = "Sorry, that emoticon was not found"
-    end
-  end
-  eng_name
-end
-
-def get_english_meaning(file_path, emoji)
-  dictionary = load_library(file_path)
-  eng_name = ""
-  dictionary.each do |name, value|
-    if emoji == value[:japanese]
-       return eng_name = name
-    else
-      eng_name = "Sorry, that emoticon was not found"
-    end
-  end
-  eng_name
-end
+#def get_english_meaning(path, emoticon)
+#load_library(path)
+#
+#
+#
+#
+#
